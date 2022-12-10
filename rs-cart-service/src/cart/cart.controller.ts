09e0d216ff35932a9721c728dd4f5152886b8c65
WizzyWu <wizzymails@gmail.com>
2022-12-10 20:12:13 +0100

    Task 8

---
+++ rs-cart-service/src/cart/cart.controller.ts
@@ -0,0 +1,104 @@
+import { Controller, Get, Delete, Put, Body, Req, Post, UseGuards, HttpStatus } from '@nestjs/common';
+
+// import { BasicAuthGuard, JwtAuthGuard } from '../auth';
+import { OrderService } from '../order';
+import { AppRequest, getUserIdFromRequest } from '../shared';
+
+import { calculateCartTotal } from './models-rules';
+import { CartService } from './services';
+
+const DEFAULT_CART_ID = '460dbc58-3dc9-40f0-a822-cfe1b7d3622e';
+
+@Controller('api/profile/cart')
+export class CartController {
+  constructor(
+    private cartService: CartService,
+    private orderService: OrderService
+  ) { }
+
+  // MEthod to simplify work with card and not implement work with users
+  async _getCard () {
+    console.log('inside get cart method');
+    // default card id to not implement work with users
+    return await this.cartService.getCart(DEFAULT_CART_ID);
+  }
+
+  // @UseGuards(JwtAuthGuard)
+  // @UseGuards(BasicAuthGuard)
+  @Get()
+  async findUserCart(@Req() req: AppRequest) {
+    console.log('inside findUserCart');
+    const cart = await this._getCard();
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+      data: { cart, total: calculateCartTotal(cart) },
+    }
+  }
+
+  // @UseGuards(JwtAuthGuard)
+  // @UseGuards(BasicAuthGuard)
+  @Put()
+  async updateUserCart(@Req() req: AppRequest, @Body() body) { // TODO: validate body payload...
+    const cart = await this.cartService.updateCart(DEFAULT_CART_ID, body);
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+      data: {
+        cart,
+        total: calculateCartTotal(cart),
+      }
+    }
+  }
+
+  // @UseGuards(JwtAuthGuard)
+  // @UseGuards(BasicAuthGuard)
+  @Delete()
+  clearUserCart(@Req() req: AppRequest) {
+    // we don't delete default card
+    //this.cartService.removeByUserId(getUserIdFromRequest(req));
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+    }
+  }
+
+  // @UseGuards(JwtAuthGuard)
+  // @UseGuards(BasicAuthGuard)
+  @Post('checkout')
+  async checkout(@Req() req: AppRequest, @Body() body) {
+    console.log('inside checkout');
+    const userId = getUserIdFromRequest(req);
+    const cart = await this._getCard();
+
+    if (!(cart && cart.items.length)) {
+      const statusCode = HttpStatus.BAD_REQUEST;
+      req.statusCode = statusCode
+
+      return {
+        statusCode,
+        message: 'Cart is empty',
+      }
+    }
+
+    const { id: cartId, items } = cart;
+    const total = calculateCartTotal(cart);
+    const order = this.orderService.create({
+      ...body, // TODO: validate and pick only necessary data
+      userId,
+      cartId,
+      items,
+      total,
+    });
+    //this.cartService.removeByUserId(userId);
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+      data: { order }
+    }
+  }
+}
